dd896855219f18e030034424d73ffcc442ea317c
mynks44 <suranimayank44@gmail.com>
2025-08-21 03:20:32 -0400

    normal fixex

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  apiBase: 'https://lingualens-pqda.onrender.com', // <-- replace after backend deploy
+  apiBase: '/api',
   firebase: {
     apiKey: "AIzaSyDOU_U6go1gjVmQ5v5aL-19jXpdj4mQ-Kg",
     authDomain: "lingualens-403ed.firebaseapp.com",
     projectId: "lingualens-403ed",
-    storageBucket: "lingualens-403ed.firebasestorage.app",
+    storageBucket: "lingualens-403ed.firebasestorage.app", 
     messagingSenderId: "887120153593",
     appId: "1:887120153593:web:a8ef042d2a1c3f64afcb28",
     measurementId: "G-MN0Y6YEGG0"
