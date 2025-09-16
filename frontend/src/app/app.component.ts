2c0fc8dd98d38ade23d8d4091b08fd294c18ea30
mynks44 <suranimayank44@gmail.com>
2025-09-16 02:57:44 -0400

    login/logout fixed

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from './core/services/auth.service';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet],
+  imports: [CommonModule, RouterLink, RouterOutlet, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- frontend/src/app/components/toolbar/toolbar.component.ts
+++ frontend/src/app/components/toolbar/toolbar.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+import { NgIf, AsyncPipe  } from '@angular/common';
+import { filter } from 'rxjs/operators';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [NgIf, AsyncPipe, RouterLink, RouterLinkActive],
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  auth = inject(AuthService);
+  router = inject(Router);
+
+  showToolbar = true;
+
+  constructor() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.showToolbar = !event.url.includes('/auth');
+      });
+  }
+
+  async logout() {
+    await this.auth.logout();
+    this.router.navigateByUrl('/auth');
+  }
+}
